b5f48ffb82ed08457db8b496800462b2f6b78c18
Joyless <65855333+Joy-less@users.noreply.github.com>
2024-11-29 21:43:12 +0000

    Add unit tests

---
+++ SQLiteSharp.Tests/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿namespace SQLiteSharp.Tests;
+
+public class UnitTest1 {
+    [Fact]
+    public void Test1() {
+        // Open a database connection
+        using SQLiteConnection Connection = new("database.db");
+
+        // Create a table for a class
+        Connection.CreateTable<ShopItem>();
+
+        // Delete all existing items in the table
+        Connection.DeleteAll<ShopItem>();
+
+        // Insert items into the table
+        Connection.Insert(new ShopItem() {
+            ItemName = "Apple",
+            Count = 10,
+        });
+        Connection.Insert(new ShopItem() {
+            ItemName = "Banana",
+            Count = 5,
+        });
+
+        // Find one item in the table matching a predicate
+        ShopItem? Apple = Connection.Find<ShopItem>(ShopItem => ShopItem.ItemName == "Apple");
+        Assert.NotNull(Apple);
+
+        // Delete an item from the table
+        Connection.Delete(Apple);
+
+        // Find several items in the table
+        List<ShopItem> Bananas = Connection.Table<ShopItem>().Where(ShopItem => ShopItem.ItemName == "Banana").ToList();
+        Assert.Single(Bananas);
+    }
+}
+
+public class ShopItem {
+    [PrimaryKey, AutoIncrement]
+    public long Id { get; set; }
+
+    [NotNull]
+    public string? ItemName { get; set; }
+
+    public long Count { get; set; }
+
+    [Ignore]
+    public int SomethingToIgnore { get; set; }
+}
