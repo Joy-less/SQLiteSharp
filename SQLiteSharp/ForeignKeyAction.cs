639f5dec7597fb613b868b2b941eafd86d82492a
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-03-18 16:27:02 +0000

    Use lowercase for `ForeignKeyAction` SQL

--- SQLiteSharp/ForeignKeyAction.cs
+++ SQLiteSharp/ForeignKeyAction.cs
@@ -9,26 +9,26 @@ public enum ForeignKeyAction {
     /// <summary>
     /// Do nothing.
     /// </summary>
-    [EnumMember(Value = "NO ACTION")]
+    [EnumMember(Value = "no action")]
     NoAction,
     /// <summary>
     /// The referenced key is prevented from being changed while it is still referenced.
     /// </summary>
-    [EnumMember(Value = "RESTRICT")]
+    [EnumMember(Value = "restrict")]
     Restrict,
     /// <summary>
     /// The references are set to null if the referenced key is changed.
     /// </summary>
-    [EnumMember(Value = "SET NULL")]
+    [EnumMember(Value = "set null")]
     SetNull,
     /// <summary>
     /// The references are set to their default value if the referenced key is changed.
     /// </summary>
-    [EnumMember(Value = "SET DEFAULT")]
+    [EnumMember(Value = "set default")]
     SetDefault,
     /// <summary>
     /// The references are also deleted/updated if the referenced key is changed.
     /// </summary>
-    [EnumMember(Value = "CASCADE")]
+    [EnumMember(Value = "cascade")]
     Cascade,
 }
