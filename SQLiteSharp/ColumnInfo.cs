80b3a33cd4b5afb0f299dcf36d286c7e51087692
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-04-08 02:25:12 +0100

    Whitespace improvements

--- SQLiteSharp/ColumnInfo.cs
+++ SQLiteSharp/ColumnInfo.cs
@@ -9,25 +9,31 @@ public record ColumnInfo {
     /// <summary>
     /// The index of the column.
     /// </summary>
-    [Alias("cid")] public int ColumnIndex { get; set; }
+    [Alias("cid")]
+    public int ColumnIndex { get; set; }
     /// <summary>
     /// The name of the column.
     /// </summary>
-    [Alias("name")] public string Name { get; set; }
+    [Alias("name")]
+    public string Name { get; set; }
     /// <summary>
     /// The exact <see href="https://sqlite.org/datatype3.html">type name</see> of the column, e.g. <c>varchar(20)</c> or <c>INTEGER</c>.
     /// </summary>
-    [Alias("type")] public string Type { get; set; }
+    [Alias("type")]
+    public string Type { get; set; }
     /// <summary>
     /// Whether the column is allowed to be <see langword="null"/>.
     /// </summary>
-    [Alias("notnull")] public bool NotNull { get; set; }
+    [Alias("notnull")]
+    public bool NotNull { get; set; }
     /// <summary>
     /// The default value for the column when inserting rows.
     /// </summary>
-    [Alias("dflt_value")] public SqliteValue DefaultValue { get; set; }
+    [Alias("dflt_value")]
+    public SqliteValue DefaultValue { get; set; }
     /// <summary>
     /// Whether the column is the primary key of the table.
     /// </summary>
-    [Alias("pk")] public bool PrimaryKey { get; set; }
+    [Alias("pk")]
+    public bool PrimaryKey { get; set; }
 }
